ec22ad165420b42c28ed66c19615ba005b3fa3ed
jbecerraromo <118853883+jbecerraromo@users.noreply.github.com>
2023-06-05 20:38:32 +0200

    Add files via upload
    Proyecto C de la piscina

---
+++ C_00/ex06/ft_print_comb2.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jesbecer <jesbecer@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/05 12:37:33 by jesbecer          #+#    #+#             */
+/*   Updated: 2023/06/05 19:59:13 by jesbecer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	int		a;
+	int		b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_putchar(48 + a / 10);
+			ft_putchar(48 + a % 10);
+			ft_putchar(' ');
+			ft_putchar(48 + b / 10);
+			ft_putchar(48 + b % 10);
+			if (!(a == 98 && b == 99))
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+	a++;
+	}
+}
